34e7d02fef2e6800efa861977cb5ce4da0ce2ea0
Smellyjellybelly <nelly.sven@gmail.com>
2023-09-14 20:45:41 +0200

    123

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from './Navbar';
 import Home from './Pages/Home';
-// import About from './Pages/About';
+import About from './Pages/About';
 import Movies from './Pages/Movies';
 
 import './App.css';
@@ -20,6 +20,9 @@ function App() {
             </Route>
             <Route path="/Pages/Movies">
               <Movies />
+            </Route>
+            <Route path="/Pages/About">
+              <About />
             </Route>
           </Switch>
         </div>
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,4 +1,4 @@
-import { Route } from "react-router-dom"
+
 
 function About() {
   return (

25b140222a4b2e0d05293f6d5adea56827690ce4
Smellyjellybelly <nelly.sven@gmail.com>
2023-09-14 21:17:05 +0200

    22

--- src/Interface/Movie.ts
+++ src/Interface/Movie.ts
@@ -1,4 +1,25 @@
-export Interface movie{
-    title: string,
-    image: string,
+interface RootObject {
+  cinema: Cinema;
+}
+
+export interface Cinema {
+  name: string;
+  movies: Movie[];
+}
+
+export interface Movie {
+  title: string;
+  duration: string;
+  shows: Show[];
+}
+
+export interface Show {
+  time: string;
+  room: string;
+  seats: Seat[];
+}
+
+export interface Seat {
+  seatNumber: string;
+  booked: boolean;
 }

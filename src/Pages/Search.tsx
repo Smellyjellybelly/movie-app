402c444bef5f32b8608e9e060e876b6b79ec8c71
Smellyjellybelly <nelly.sven@gmail.com>
2023-09-18 14:25:31 +0200

    search function works

--- src/Pages/Search.tsx
+++ src/Pages/Search.tsx
@@ -1,60 +1,31 @@
-import React, { useState, useEffect, ChangeEvent } from 'react';
+import { Cinema } from '../Interface/interface';
+import JsonData from '../movies.json';
+import React from 'react';
+import { useState } from 'react';
 
-// Import your interface definitions
-import { RootObject, Movie, Cinema } from '../Interface/interface'; 
+const data: Cinema = JsonData.cinema;
 
 function CinemaSearch() {
-    const [cinemaData, setCinemaData] = useState<Cinema | null>(null);
-    const [searchQuery, setSearchQuery] = useState('');
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('movies.json');
-                if (!response.ok) {
-                    throw new Error('Problem fetching data');
-                }
-                const data: RootObject = await response.json(); // Parse with RootObject type
-                setCinemaData(data.cinema);
-            } catch (error) {
-                console.error(error);
-                // Handle error state here, if needed
-            }
-        };
-
-        fetchData();
-    }, []);
-
-    const handleSearchInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setSearchQuery(e.target.value);
-    };
-
-    let filteredMovies: Movie[] = [];
-
-    if (cinemaData) {
-        filteredMovies = cinemaData.movies.filter((movie) =>
-            movie.title.toLowerCase().includes(searchQuery.toLowerCase())
-        );
-    }
+    const [searchTerm, setSearchTerm] = useState('')
 
     return (
-        <div>
-            <h1>Cinema Search</h1>
-            <input
-                type="text"
-                placeholder="Search for a movie..."
-                value={searchQuery}
-                onChange={handleSearchInputChange}
-            />
-            <ul>
-                {filteredMovies.map((movie) => (
-                    <li key={movie.title}>
-                        {movie.title} ({movie.duration})
-                    </li>
-                ))}
-            </ul>
+        <div className="search">
+            <input type="text" placeholder='Sök' onChange={event => { setSearchTerm(event.target.value) }} />
+            {data.movies.filter((movies) => {
+                if (searchTerm == ""){
+                return movies
+            } else if (movies.title.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())) {
+                return movies
+            }
+            }).map((movies) => {
+                return <div className='search-res'>
+                    <img src={movies.picture} alt="" />
+                    <p>{movies.title}</p>
+                </div>
+            })}
         </div>
     );
 }
 
-export default CinemaSearch;
+export default CinemaSearch
